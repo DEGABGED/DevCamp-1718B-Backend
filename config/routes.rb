ee8e10e32d6761636255a65d236700fd21efce8d
Francis dela Cruz <franciszac.dlc@gmail.com>
2018-03-16 00:41:19 +0800

    Routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :articles do
+    resource :comments, only: :create
+  end
+
+  root 'articles#index'
+
+  #get 'login' => 'user_sessions#new'
+  #post 'login' => 'user_session#create'
+  #delete 'logout' => 'user_sessions#destroy'
 end
