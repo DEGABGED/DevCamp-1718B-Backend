78a91eadb10cc948e61b3644535c4d364cdf0619
Francis dela Cruz <franciszac.dlc@gmail.com>
2018-03-17 01:27:07 +0800

    Intermediate controller methods, view forms

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
     @articles = Article.all
@@ -13,6 +13,45 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+  end
+
+  def create
+    @article = Article.new(article_params)
+    respond_to do |format|
+      if @article.save
+          format.html { redirect_to @article,
+                      notice: 'Article was successfully created.' }
+          format.json { render action: 'show',
+                      status: :created, location: @article }
+      else
+          format.html { render action: 'new' }
+          format.json { render json: @article.errors,
+                      status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @article.update(article_params)
+          format.html { redirect_to @article,
+                      notice: 'Article was successfully updated.' }
+          format.json { render action: 'show',
+                      status: :ok, location: @article }
+      else
+          format.html { render action: 'edit' }
+          format.json { render json: @article.errors,
+                      status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @article.destroy
+    respond_to do |format|
+      format.html { redirect_to articles_url }
+      format.json { head :no_content }
+    end
   end
 
   private
